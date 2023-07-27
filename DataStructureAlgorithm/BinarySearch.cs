b5226778f90b120ef9215e6d70449772bf0dc3fa
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-27 10:36:28 +0530

    [Shrey] Add : Binary search feature

---
+++ DataStructureAlgorithm/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class BinarySearch
+    {
+        public void ReadTextFileAndSearch(string filePath)
+        {
+            string readAllText = File.ReadAllText(filePath);
+            string[] words = readAllText.Split(",");
+            Console.WriteLine("Enter key to search");
+            string key = Console.ReadLine();
+            bool isFind = false;
+            foreach (var data in words)
+            {
+                if(data.Equals(key))
+                {
+                    isFind = true;
+                }
+                
+            }
+            if(isFind)
+            {
+                Console.WriteLine("Search element {0} found",key);
+            }
+            else
+            {
+                Console.WriteLine("Search element not found");
+            }
+
+        }
+    }
+}
--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using DataStructureAlgorithm;
+
+public class Program
 {
-    private static void Main(string[] args)
+    static string searchFilePath = @"D:\Bridgelabz Problem statement\DataStructureAlgorithm\DataStructureAlgorithm\Files\Search.txt";
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Data Structure");
+        Console.WriteLine("Data Structure\n");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Enter a option \n1.BinarySearch \n2.Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    BinarySearch search = new BinarySearch();
+                    search.ReadTextFileAndSearch(searchFilePath);
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+            }
+        }
+
+
     }
 }
